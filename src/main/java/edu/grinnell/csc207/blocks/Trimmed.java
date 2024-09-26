f7f9b82a921bde4bf9658f9217d99a9acf08a51a
NICOLE_GORRELL <GORRELLN@grinnell.edu>
2024-09-26 18:03:35 -0500

    trimmed wip

--- src/main/java/edu/grinnell/csc207/blocks/Trimmed.java
+++ src/main/java/edu/grinnell/csc207/blocks/Trimmed.java
@@ -4,7 +4,9 @@ package edu.grinnell.csc207.blocks;
  * A trimmed ASCII block.
  *
  * @author Samuel A. Rebelsky
- * @author Your Name Here
+ * @author Nicole
+ * @author Tiffany
+ * @author Harrison
  */
 public class Trimmed implements AsciiBlock {
   // +--------+------------------------------------------------------------
@@ -44,15 +46,17 @@ public class Trimmed implements AsciiBlock {
    * Build a new block with the specified contents.
    *
    * @param original
-   *   The original block.
+   *                      The original block.
    * @param horiz
-   *   How the trimmed block is horizontally aligned on the original.
+   *                      How the trimmed block is horizontally aligned on the
+   *                      original.
    * @param vert
-   *   How the trimmed block is vertically aligned on the original.
+   *                      How the trimmed block is vertically aligned on the
+   *                      original.
    * @param trimmedWidth
-   *   The width of the trimmed block.
+   *                      The width of the trimmed block.
    * @param trimmedHeight
-   *   The height of the trimmed block.
+   *                      The height of the trimmed block.
    */
   public Trimmed(AsciiBlock original, HAlignment horiz, VAlignment vert,
       int trimmedWidth, int trimmedHeight) {
@@ -78,7 +82,40 @@ public class Trimmed implements AsciiBlock {
    *   If the row is invalid.
    */
   public String row(int i) throws Exception {
-    throw new Exception("Not yet implemented"); // STUB
+    if(this.height > this.block.height() || this.width > this.block.width()) {
+      throw new Exception("Trim dimensions exceed those of the input block. Try again.");
+    } // if
+
+    String result = "";
+
+    // if...else block goes through each possible alignment combinations to trim from this.block
+    if (this.halign == HAlignment.LEFT && this.valign == VAlignment.TOP) {
+      result = this.block.row(i).substring(0, this.width);
+    } else if (this.halign == HAlignment.CENTER && this.valign == VAlignment.TOP) {
+      result = this.block.row(i).substring((this.block.width() - 1) / 2, 
+                                             ((this.block.width() - 1) / 2) + 2);
+    } else if (this.halign == HAlignment.RIGHT && this.valign == VAlignment.TOP) {
+      result = this.block.row(i).substring(this.block.width() - this.width, this.block.width()); 
+    } else if (this.halign == HAlignment.LEFT && this.valign == VAlignment.CENTER) {
+      result = this.block.row((this.block.height() - 1) / 2).substring((this.block.width() - 1) / 2, 
+                                                                         ((this.block.width() - 1) / 2) + 1);
+    } else if (this.halign == HAlignment.CENTER && this.valign == VAlignment.CENTER) {
+      result = this.block.row((this.block.height() - 1) / 2).substring((this.block.width() - 1) / 2, 
+                                                                         ((this.block.width() - 1) / 2) + 2);
+    } else if (this.halign == HAlignment.RIGHT && this.valign == VAlignment.CENTER) {
+      result = this.block.row((this.block.height() - 1) / 2).substring(this.block.width() - this.width, this.block.width());
+    } else if (this.halign == HAlignment.LEFT && this.valign == VAlignment.BOTTOM) {
+      result = this.block.row(this.block.height() - this.height).substring(0, this.width);
+    } else if (this.halign == HAlignment.CENTER && this.valign == VAlignment.BOTTOM) {
+      result = this.block.row(this.block.height() - this.height).substring((this.block.width() - 1) / 2, 
+                                                                             ((this.block.width() - 1) / 2) + 2);
+    } else if (this.halign == HAlignment.RIGHT && this.valign == VAlignment.BOTTOM) {
+      result = this.block.row(this.block.height() - this.height).substring(this.block.width() - this.width, this.block.width());
+      //result = result.concat(this.block.row(this.block.height() - 1)); 
+  
+    } // if...else
+
+    return result;
   } // row(int)
 
   /**
@@ -87,7 +124,7 @@ public class Trimmed implements AsciiBlock {
    * @return the number of rows
    */
   public int height() {
-    return 0;   // STUB
+    return this.height;
   } // height()
 
   /**
@@ -96,17 +133,17 @@ public class Trimmed implements AsciiBlock {
    * @return the number of columns
    */
   public int width() {
-    return 0;   // STUB
+    return this.width;
   } // width()
 
   /**
    * Determine if another block is structurally equivalent to this block.
    *
    * @param other
-   *   The block to compare to this block.
+   *              The block to compare to this block.
    *
    * @return true if the two blocks are structurally equivalent and
-   *    false otherwise.
+   *         false otherwise.
    */
   public boolean eqv(AsciiBlock other) {
     return other instanceof Trimmed && this.eqv((Trimmed) other);
@@ -116,16 +153,16 @@ public class Trimmed implements AsciiBlock {
    * Determine if another block is structurally equivalent to this block.
    *
    * @param other
-   *   The block to compare to this block.
+   *              The block to compare to this block.
    *
    * @return true if the two blocks are structurally equivalent and
-   *    false otherwise.
+   *         false otherwise.
    */
   public boolean eqv(Trimmed other) {
     return other.width() == this.width() &&
-            other.height() == this.height() &&
-            other.halign == this.halign &&
-            other.valign == this.valign &&
-            other.block.eqv(this.block);
+        other.height() == this.height() &&
+        other.halign == this.halign &&
+        other.valign == this.valign &&
+        other.block.eqv(this.block);
   } // eqv(AsciiBlock)
 } // class Trimmed
