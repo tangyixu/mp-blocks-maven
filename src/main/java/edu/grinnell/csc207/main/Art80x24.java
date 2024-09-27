56a5062c76ac2b5ab79355140a0f589a5223113a
tangyixu <tangyixu@grinnell.edu>
2024-09-26 19:59:50 -0500

    UnfinishedArt

--- src/main/java/edu/grinnell/csc207/main/Art80x24.java
+++ src/main/java/edu/grinnell/csc207/main/Art80x24.java
@@ -22,9 +22,17 @@ public class Art80x24 {
    */
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
-    AsciiBlock art = new Rect('^', 80, 24);
-    Circle test = new Circle('C',3);
-    AsciiBlock.print(pen, new Surrounded(test, 'x'));
+    Circle mountainbase = new Circle('*',11) ;
+    AsciiBlock sky = new Rect(' ', 80, 10); 
+    AsciiBlock grass = new Rect('^', 80, 1);
+    //Circle emptyheart = new Circle('+',2);
+    //AsciiBlock linedheart = new Surrounded(emptyheart,'.');
+    Trimmed trimmedbase = new Trimmed(mountainbase,HAlignment.LEFT, VAlignment.TOP, 11, 4);
+    AsciiBlock.print(pen, trimmedbase);
+    //AsciiBlock.print(pen, mountainbase);
+    AsciiBlock.print(pen, sky);
+    AsciiBlock.print(pen, grass);
+    
 
     pen.close();
   } // main(String[])
